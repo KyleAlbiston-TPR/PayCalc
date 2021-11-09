6f22362963313b67f2e00d6b44c83596e54db922
Kyle Albiston <Kyle.Albiston@tpr.gov.uk>
2021-11-09 10:49:08 +0000

    folder structure

---
+++ PayCalc/Data/ITempEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalc
+{
+    class ITempEmployeeRepository
+    {
+        //maybe use idk yet
+    }
+}
